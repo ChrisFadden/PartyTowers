aab6e344179b2ba380256d04aaf8614d20df88c5
Marcus Godwin <marcusgodwin@hotmail.com>
2015-09-25 21:13:00 -0400

    Ignore generated executable. Link to SDL Net.
    Also some CMake file fixes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <SDL2/SDL_net.h>
 
 int main() {
     std::cout << "HELLO PARTY TOWERS!!!!\n";
+    if(SDL_Init(0) == -1) {
+        std::cout << "ERROR, SDL_Init\n";
+        return -1;
+    }
+
+    if(SDLNet_Init() == -1) {
+        std::cout << "ERROR, SDLNet_Init\n";
+        return -1;
+    }
+
+    std::cout << "WOO SDL2_Net!!!!\n";
     return 0;
 }
